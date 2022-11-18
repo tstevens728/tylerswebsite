cdea8a61e036a4e25d8960887541e433263a67ff
tstevens <tstevens@punchey.com>
2022-11-18 03:42:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <div className="header" style={styles.header}>
+        <h1 styles={styles.nameText}>TylerStevens._</h1>
+        <div style={styles.navButtonContainer}>
+          <a href="">
+            <h1 id="hover-content">About Me</h1>
+          </a>
+          <a href="">
+            <h1 id="hover-content">Expierence</h1>
+          </a>
+          <a href="">
+            <h1 id="hover-content">Projects</h1>
+          </a>
+          <a href="">
+            <h1 id="hover-content">Contact Me!</h1>
+          </a>
+        </div>
+      </div>
+      <div style={styles.bigContainer}></div>
     </div>
   );
 }
 
 export default App;
+
+const styles = {
+  bigContainer: {
+    height: "5000px",
+  },
+  container: {
+    height: "100%",
+
+    width: "100%",
+  },
+  navButtonContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    width: "1400px",
+  },
+  header: {
+    display: "flex",
+    alignItems: "center",
+    paddingLeft: "30px",
+    height: "100px",
+    marginTop: "20px",
+    borderTopRightRadius: "20px",
+    borderBottomRightRadius: "20px",
+  },
+  nameText: {
+    fontWeight: "40px",
+    fontFamily: "Orbitron",
+  },
+};
